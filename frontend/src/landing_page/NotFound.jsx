1d40c39836408b62e626e9e72d5c8eb74ad849cd
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2025-03-05 23:12:44 +0530

    created not found page

---
+++ frontend/src/landing_page/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <h1 className="mt-2 mb-4">404 Not Found</h1>
+        <p className="mb-4">
+          Sorry, the page you are looking for does not exists.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
